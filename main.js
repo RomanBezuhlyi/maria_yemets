46bdb11700b976b2fd2737ec7e6292e689263861
Andrew <wotriger@gmail.com>
2026-07-05 17:04:32 +0200

    Created full presets.html with adaptation

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ import '@/styles/main.scss'
 import Lenis from 'lenis'
 import 'lenis/dist/lenis.css'
 
-import { Fancybox } from '@fancyapps/ui/dist/fancybox/'
-import '@fancyapps/ui/dist/fancybox/fancybox.css'
-import { initSliders } from '@/js/modules/slider'
-import { initPhoneMask } from '@/js/modules/phoneMask'
 import { initAccordion } from '@/js/modules/accordion'
-import { initMenu } from '@/js/modules/menu'
-import { initDropdown } from '@/js/modules/dropdown'
 import { initScrollAnimations } from '@/js/modules/animation'
+import { initDropdown } from '@/js/modules/dropdown'
 import { initHeaderScroll } from '@/js/modules/headerScroll'
 import { initHeroSlider } from '@/js/modules/heroSlider'
 import { initBookTabs } from '@/js/modules/initBookTabs'
+import { initMenu } from '@/js/modules/menu'
+import { initPhoneMask } from '@/js/modules/phoneMask'
+import { initSliders } from '@/js/modules/slider'
+import { Fancybox } from '@fancyapps/ui/dist/fancybox/'
+import '@fancyapps/ui/dist/fancybox/fancybox.css'
+import { initPresetsComparison } from './src/js/modules/comparison'
 
 document.addEventListener('DOMContentLoaded', () => {
 	const lenis = new Lenis({
@@ -44,4 +45,5 @@ document.addEventListener('DOMContentLoaded', () => {
 		activeClass: 'is-open',
 	})
 	initBookTabs()
+	initPresetsComparison()
 })
